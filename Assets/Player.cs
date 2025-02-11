46546a310c6e4fa20405ef5b8fa6e498bb7ea5cb
SikasiKasi <SikasiKasi029@gmail.com>
2025-02-11 20:13:02 +0900

    キー移動実装

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector3(x, y) * Time.deltaTime * this.speed);
+    }
+}
